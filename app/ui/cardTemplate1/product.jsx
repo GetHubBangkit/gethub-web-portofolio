a45cb3b9277a3aee380e7faafe68f152aef06746
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-05-03 21:23:53 +0700

    feat : "fix the truncate tailwind for height and wrap with grid item"

--- app/ui/cardTemplate1/product.jsx
+++ app/ui/cardTemplate1/product.jsx
@@ -6,10 +6,10 @@ import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 
 // komponen product card 
 const ProductCard = ({ product }) => (
-  <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300' style={{ boxSizing: 'border-box' }}>
+  <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 ' style={{ boxSizing: 'border-box' }}>
     <img className='rounded-2xl rounded-b-none bg-white w-44 h-32' src={product.image} alt='Product' />
     <h1 className='mt-4 font-bold text-xs text-black'>{product.name}</h1>
-    <p className='text-black text-xs' style={{ whiteSpace: 'normal' }}>{product.description}</p>
+    <p className='text-black text-xs line-clamp-3'>{product.description}</p>
   </div>
 );
 
@@ -32,13 +32,13 @@ const Product = ({products}) => {
     <div className='flex justify-start flex-col items-start pb-10 w-full'>
       <div className='flex w-full items-center pb-4 justify-start'>
         <img src="card1/gethub.png" alt="" className='h-[30px] w-[30px]' />
-        <div className='text-xl font-semibold text-black w-full flex items-center justify-start ml-2 h-full '>
+        <div className='text-xl font-bold text-black w-full flex items-center justify-start ml-2 h-full ' style={{ color:'#0D6375' }}>
           Produk / Jasa
         </div>
       </div>
       <div className='flex relative items-center  h-auto w-full'>
-      <MdChevronLeft onClick={sliderLeft} size={40} color='black' className='opacity-50 cursor-pointer hover:opacity-100' />
-        <div id="slider" className='overflow-x-scroll h-full w-full whitespace-nowrap scroll scroll-smooth scrollbar-hide'>
+      <MdChevronLeft onClick={sliderLeft} size={20} color='black' className='opacity-50 cursor-pointer hover:opacity-100 bg-transparent p-0' />
+        <div id="slider" className='grid grid-flow-col overflow-x-scroll  w-full scroll scroll-smooth scrollbar-hide'>
           {products.map((product, index) => <ProductCard key={index} product={product} />)}
         </div> 
         <MdChevronRight onClick={sliderRight} size={40} color='black' className='opacity-50 cursor-pointer hover:opacity-100'/>
--- app/ui/cardTemplate1/sosmedLink.jsx
+++ app/ui/cardTemplate1/sosmedLink.jsx
@@ -7,7 +7,7 @@ import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 // buttonlink component
 const ButtonLink = ({ sosmedLink }) => (
   <a href={ sosmedLink.link }>
-    <button className=' bg-white text-white  w-[144px] h-[52px] rounded-3xl flex justify-center items-center shadow'>
+    <button className='     w-[144px] h-[52px] rounded-3xl flex justify-center items-center shadow'>
       <img src={
         sosmedLink.category === 'tiktok' ? 'tiktok.png' :
         sosmedLink.category === 'tokopedia' ? 'tokped.png' :
@@ -35,16 +35,16 @@ const sosmedLink = ({ links }) => {
 
   
   return (
-    <div className='flex justify-start flex-col items-start w-full pb-6'>
+    <div className='flex justify-start flex-col items-start w-full pb-6 '>
         <div className='flex w-full items-center pb-4 justify-start'>
           <img src="card1/gethub.png" alt="" className='h-[30px] w-[30px]'/>
-          <div className='text-xl font-semibold text-black w-full flex items-center justify-start ml-2 h-full '>
-            GetHub Link
+          <div className='text-xl font-bold text-black w-full flex items-center justify-start ml-2 h-full '>
+            Get <span className='text-black text-xl' style={{color: '#0D6375'}}> Hub Link </span>
           </div>
         </div>
         <div className='flex relative items-center h-auto w-full  py-4'>
           <MdChevronLeft onClick={sliderLeft} size={20} color='black' className='opacity-50 cursor-pointer hover:opacity-100' />
-            <div id="sliderLink" className='flex overflow-x-scroll w-full h-full whitespace-nowrap scroll scroll-smooth scrollbar-hide gap-2'>
+            <div id="sliderLink" className=' flex overflow-x-scroll w-full h-full whitespace-nowrap scroll scroll-smooth scrollbar-hide gap-2'>
               {links.map((link, index) => (
                 <ButtonLink key={index} sosmedLink={link} />
               ))}
