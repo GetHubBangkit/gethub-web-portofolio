fcf5e5314d46318cf0a25c74dd83eb8e6e3bc08d
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-05-31 16:34:01 +0700

    Refactor component and page styles for consistency

--- app/components/utils/productUtils.jsx
+++ app/components/utils/productUtils.jsx
@@ -14,7 +14,7 @@ const productCard = ({ product, priceColor }) => (
     <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
       <img className='rounded-2xl rounded-b-none bg-white w-44 h-32 pb-1' src={product.image_url} alt='Product' />
       {/* <h1 style={{ color: priceColor }} className='mt-4 font-bold text-xs'>Rp. {product.price}</h1> */}
-      <h1 className=' font-bold text-xs text-black mb-2'>{product.name}</h1>
+      <h1 className=' font-bold text-xs text-black mb-2 pt-2'>{product.name}</h1>
       <p className='text-black text-xs line-clamp-3'>{product.description}</p>
     </div>
 );
@@ -23,7 +23,7 @@ const productCard2 = ({ product, priceColor }) => (
     <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
       <img className='rounded-2xl rounded-b-none bg-white w-44 h-32 pb-1' src={product.image_url} alt='Product' />
       {/* <h1 style={{ color: priceColor }} className='mt-4 font-bold text-xs'>Rp. {product.price}</h1> */}
-      <h1 className=' font-bold text-xs text-black mb-2'>{product.name}</h1>
+      <h1 className=' font-bold text-xs text-black mb-2 pt-2'>{product.name}</h1>
       <p className='text-black text-xs line-clamp-3'>{product.description}</p>
     </div>
   );
