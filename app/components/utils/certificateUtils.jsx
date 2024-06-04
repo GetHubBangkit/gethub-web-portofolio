300e7be7cf4d34116659024fac96a070ec73a3f1
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-06-04 16:18:53 +0700

    add category certification

--- app/components/utils/certificateUtils.jsx
+++ app/components/utils/certificateUtils.jsx
@@ -14,8 +14,8 @@ const certificateCard = ({ certificate, priceColor }) => (
     <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
       <img className='rounded-2xl rounded-b-none bg-white w-44 h-32 pb-1' src={certificate.image} alt='Certificate' />
       {/* <h1 style={{ color: priceColor }} className='mt-4 font-bold text-xs'>Rp. {product.price}</h1> */}
-      <h1 className=' font-bold text-xs text-black mb-2 pt-2'>{certificate.title}</h1>
-      <p className='text-black text-xs line-clamp-3'>{certificate.description}</p>
+      <h1 className=' font-bold text-xs text-black mb-1 pt-2'>{certificate.title}</h1>
+      <p className='text-black text-xs line-clamp-3'>Category : {certificate.category_name}</p>
     </div>
 );
   
@@ -23,8 +23,8 @@ const certificateCard2 = ({ certificate, priceColor }) => (
     <div className='w-44 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
       <img className='rounded-2xl rounded-b-none bg-white w-44 h-32 pb-1' src={certificate.image} alt='Certificate' />
       {/* <h1 style={{ color: priceColor }} className='mt-4 font-bold text-xs'>Rp. {product.price}</h1> */}
-      <h1 className=' font-bold text-xs text-black mb-2 pt-2'>{certificate.title}</h1>
-      <p className='text-black text-xs line-clamp-3'>{certificate.description}</p>
+      <h1 className=' font-bold text-xs text-black mb-1 pt-2'>{certificate.title}</h1>
+      <p className='text-black text-xs line-clamp-3'>Category : {certificate.category_name}</p>
     </div>
   );
 
