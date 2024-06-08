7488d0a6cbe06d2665309f25d01efc49eddb3ec6
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-06-08 11:06:42 +0700

    refactor: Update image URLs in linkUtils.jsx for Figma, GitHub, Instagram, LinkedIn, GitLab, Jupyter Notebook, YouTube, TikTok, Tokopedia, and Shopee categories

--- app/components/utils/linkUtils.jsx
+++ app/components/utils/linkUtils.jsx
@@ -14,10 +14,17 @@ export const ButtonLinkHorizontal = ({ sosmedLink }) => (
   <a href={ sosmedLink.link }>
     <button className='w-[144px] h-[52px] rounded-3xl flex justify-center items-center shadow'>
       <img src={
-        sosmedLink.category === 'tiktok' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png':
-        sosmedLink.category === 'tokopedia' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png':
-        sosmedLink.category === 'shopee' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png':
-        'tiktok.png'
+        sosmedLink.category === 'figma' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/figma2.png' :
+        sosmedLink.category === 'github' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/github2.png' :
+        sosmedLink.category === 'instagram' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/instagram2.png' :
+        sosmedLink.category === 'linkedin' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/linkedin2.png' :
+        sosmedLink.category === 'gitlab' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/gitlab2.png' :
+        sosmedLink.category === 'jupyternotebook' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/jupyternotebook2.png' :
+        sosmedLink.category === 'youtube' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/youtube2.png' :
+        sosmedLink.category === 'tiktok' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/tiktok2.png' :
+        sosmedLink.category === 'tokopedia' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/tokopedia2.png' :
+        sosmedLink.category === 'shopee' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png' :
+        'notfound.png'
       }  className='' />
     </button>
   </a>
@@ -27,10 +34,17 @@ export const ButtonLinkVertical = ({ sosmedLink, color }) => (
   <a href={ sosmedLink.link }>
     <button style={{ backgroundColor: color }} className="w-[344px] h-[52px] rounded-3xl flex justify-center items-center shadow">
       <img src={
-        sosmedLink.category === 'tiktok' ? 'tiktok.png' :
-        sosmedLink.category === 'tokopedia' ? 'tokped.png' :
-        sosmedLink.category === 'Shopee' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png' :
-        'tiktok.png'
+        sosmedLink.category === 'figma' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/figma2.png' :
+        sosmedLink.category === 'github' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/github2.png' :
+        sosmedLink.category === 'instagram' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/instagram2.png' :
+        sosmedLink.category === 'linkedin' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/linkedin2.png' :
+        sosmedLink.category === 'gitlab' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/gitlab2.png' :
+        sosmedLink.category === 'jupyternotebook' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/jupyternotebook2.png' :
+        sosmedLink.category === 'youtube' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/youtube2.png' :
+        sosmedLink.category === 'tiktok' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/tiktok2.png' :
+        sosmedLink.category === 'tokopedia' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/tokopedia2.png' :
+        sosmedLink.category === 'shopee' ? 'https://storage.googleapis.com/gethub_bucket/link_category/logo%202/shopee2.png' :
+        'notfound.png'
       }  className='object-contain p-2' />
     </button>
   </a>
