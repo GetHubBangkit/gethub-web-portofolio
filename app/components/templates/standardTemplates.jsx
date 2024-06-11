221b91be2d71a7dfee4a599c459fb384f0361328
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-06-11 21:35:02 +0700

    back to mx-4

--- app/components/templates/standardTemplates.jsx
+++ app/components/templates/standardTemplates.jsx
@@ -10,7 +10,7 @@ import CertificateComponent from '../section/certificateModel';
 export const StandardTemplate1 = (data) => {
     return (
         <div id="#forfont" className="flex justify-center h-screen latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel1 {...{data : data, textColor: 'black'}} />
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <AboutComponents.aboutModel1 {...data} />
@@ -26,7 +26,7 @@ export const StandardTemplate1 = (data) => {
 export const StandardTemplate2 = (data) => {    
     return (
         <div id="#for font" className="flex justify-center h-auto latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel1 {...{data : data, textColor: 'white'}} />  
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <AboutComponents.aboutModel1 {...data} />
@@ -42,7 +42,7 @@ export const StandardTemplate2 = (data) => {
 export const StandardTemplate3 = (data) => {
     return (
         <div id="#for font" className="flex justify-center h-auto latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel2 {...{data : data, textColor: 'white', qrcolor : '#6207E4'}} />
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <AboutComponents.aboutModel1 {...data} />
@@ -58,7 +58,7 @@ export const StandardTemplate3 = (data) => {
 export const StandardTemplate4 = (data) => {
     return (
         <div id="#for font" className="flex justify-center h-auto latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel1 {...{data : data, textColor: 'white', qrcolor: '#8F8C89' }} />
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <LinksComponents.linksModel3 {...data} /> 
@@ -74,7 +74,7 @@ export const StandardTemplate4 = (data) => {
 export const StandardTemplate5 = (data) => {
     return (
         <div id="#for font" className="flex justify-center h-auto latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4  lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel1 {...{data : data, textColor: 'black', qrcolor: '#FFD7D7'}} />
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <LinksComponents.linksModel3 {...data} /> 
@@ -90,7 +90,7 @@ export const StandardTemplate5 = (data) => {
 export const StandardTemplate6 = (data) => {
     return (
         <div id="#for font" className="flex justify-center h-auto latar">
-            <div className='h-auto flex flex-col items-center sm:mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
+            <div className='h-auto flex flex-col items-center mx-4 lg:p-0 bg-white' style={{ width: '430px', minHeight: '110vh' }}>
                 <CardComponents.cardModel1 {...{data : data, textColor: 'black'}} />
                 <div className='flex flex-col items-center w-full px-8 relative'>
                     <AboutComponents.aboutModel2 {...data} />
