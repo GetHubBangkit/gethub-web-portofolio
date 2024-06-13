98da77c7f5b181ab39d4b40b01f34c9645d679bd
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-06-13 13:35:25 +0700

    refactor: Update productCard2 component with responsive width and remove unused styles 44 to 34

--- app/components/utils/productUtils.jsx
+++ app/components/utils/productUtils.jsx
@@ -20,7 +20,7 @@ const productCard = ({ product, priceColor }) => (
 );
   
 const productCard2 = ({ product, priceColor }) => (
-    <div className='w-44 px-3 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
+    <div className='w-34 inline-block h-auto cursor-pointer hover:scale-105 p-2 ease-in-out duration-300 '>
       <img className='rounded-2xl rounded-b-none bg-white w-44 h-32 pb-1' src={product.image_url} alt='Product' />
       {/* <h1 style={{ color: priceColor }} className='mt-4 font-bold text-xs'>Rp. {product.price}</h1> */}
       <h1 className=' font-bold text-xs text-black mb-2 pt-2'>{product.name}</h1>
